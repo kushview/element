04df94a95e341e3d616746adfdaac90a7daec237
Michael Fisher <mfisher@kushview.net>
2017-04-08 19:21:08 -0400

    Updating License Information in Code

--- project/Source/Globals.h
+++ project/Source/Globals.h
@@ -1,24 +1,10 @@
 /*
     Globals.h - This file is part of Element
-    -2016  Kushview, LLC.  All rights reserved.
-
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+    Copyright (C) 2016-2017 Kushview, LLC.  All rights reserved.
 */
 
-#ifndef ELEMENT_GLOBALS_H
-#define ELEMENT_GLOBALS_H
+#ifndef EL_GLOBALS_H
+#define EL_GLOBALS_H
 
 #include "element/Juce.h"
 #include "engine/AudioEngine.h"
@@ -70,4 +56,4 @@ private:
 
 }
 
-#endif // ELEMENT_GLOBALS_H
+#endif // EL_GLOBALS_H
