314f0db1835cd22547f6a8563e1fd156f05c9623
Michael Fisher <mfisher@kushview.net>
2017-04-17 15:36:12 -0400

    Show stock plugin manager from main menu

--- project/Source/gui/MainMenu.h
+++ project/Source/gui/MainMenu.h
@@ -1,20 +1,6 @@
 /*
     MainMenu.h - This file is part of Element
     Copyright (C) 2016 Kushview, LLC.  All rights reserved.
-
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 */
 
 #ifndef ELEMENT_MAIN_MENU_H
@@ -138,7 +124,7 @@ private:
     void buildWindowMenu (PopupMenu& menu)
     {
         // const bool isOpen = owner.app().isWindowOpen (ELEMENT_PLUGIN_MANAGER);
-        // menu.addItem (999, "Plugin Manager", true, isOpen);
+        menu.addCommandItem (&cmd, Commands::showPluginManager);
     }
     
     void buildHelpMenu (PopupMenu& menu)
