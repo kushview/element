bbc659e9db152b9a4775811393afd73bfcb65f6c
Michael Fisher <mfisher@kushview.net>
2017-04-17 15:44:51 -0400

    Resave jucer components

--- project/Source/gui/NewSessionView.h
+++ project/Source/gui/NewSessionView.h
@@ -47,8 +47,8 @@ public:
     //[UserMethods]     -- You can add your own custom methods in this section.
     //[/UserMethods]
 
-    void paint (Graphics& g);
-    void resized();
+    void paint (Graphics& g) override;
+    void resized() override;
 
 
 
