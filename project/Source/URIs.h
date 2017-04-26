8f8d0a8f50c986fbe197a7917f6738845ea0f20a
Michael Fisher <mfisher@kushview.net>
2017-04-26 06:54:01 -0400

    Update Simple URI defs

--- project/Source/URIs.h
+++ project/Source/URIs.h
@@ -3,18 +3,18 @@
     Copyright (C) 2016 Kushview, LLC.  All rights reserved.
 */
 
-#ifndef ELEMENT_APP_URIS_H
-#define ELEMENT_APP_URIS_H
+#ifndef EL_APP_URIS_H
+#define EL_APP_URIS_H
 
-#define ELEMENT_URI "http://bketech.com/ns/beatthang"
+#define ELEMENT_URI "https://kushview.net/ns/element"
 
-#define ELEMENT_ABOUT_BOX       ELEMENT_URI "#aboutBox"
-#define ELEMENT_PREFERENCES     ELEMENT_URI "#preferences"
-#define ELEMENT_LEGACY_WINDOW   ELEMENT_URI "#legacyWindow"
-#define ELEMENT_PLUGIN_MANAGER  ELEMENT_URI "#pluginManager"
+#define ELEMENT_ABOUT_BOX        ELEMENT_URI "#aboutBox"
+#define ELEMENT_PREFERENCES      ELEMENT_URI "#preferences"
+#define ELEMENT_LEGACY_WINDOW    ELEMENT_URI "#legacyWindow"
+#define ELEMENT_PLUGIN_MANAGER   ELEMENT_URI "#pluginManager"
 
 namespace Element {
     
 }
 
-#endif // ELEMENT_APP_URIS_H
+#endif // EL_APP_URIS_H
