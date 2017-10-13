be8a05dde168807643f94d1764b61adcba899b43
Michael Fisher <mfisher@kushview.net>
2017-10-13 04:55:50 -0400

    Version bump

--- project/JuceLibraryCode/JuceHeader.h
+++ project/JuceLibraryCode/JuceHeader.h
@@ -45,7 +45,7 @@
 namespace ProjectInfo
 {
     const char* const  projectName    = "Element";
-    const char* const  versionString  = "0.6.1";
-    const int          versionNumber  = 0x601;
+    const char* const  versionString  = "0.6.2";
+    const int          versionNumber  = 0x602;
 }
 #endif
