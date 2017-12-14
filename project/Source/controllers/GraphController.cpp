9f67a51b71299cd8aec812fbf65088d37e13b90d
Michael Fisher <mfisher@kushview.net>
2017-12-13 22:46:22 -0500

    Remove bus forced changes. Added ioButton to FilterComponent

--- project/Source/controllers/GraphController.cpp
+++ project/Source/controllers/GraphController.cpp
@@ -40,26 +40,6 @@ GraphNode* GraphController::createFilter (const PluginDescription* desc, double 
     if (instance != nullptr)
     {
         instance->enableAllBuses();
-        
-        // try to find the greatest supported discrete layout
-        for (int ins = 9; --ins >= 1;)
-        {
-            instance->getTotalNumInputChannels();
-            bool success = false;
-            for (int outs = 9; --outs >= 1;)
-            {
-                AudioProcessor::BusesLayout layout;
-                layout.inputBuses.add (AudioChannelSet::discreteChannels (ins));
-                layout.outputBuses.add (AudioChannelSet::discreteChannels (outs));
-                if (instance->checkBusesLayoutSupported (layout))
-                    success = instance->setBusesLayoutWithoutEnabling (layout);
-                if (success)
-                    break;
-            }
-            if (success) 
-                break;
-        }
-        
         node = processor.addNode (instance, nodeId);
     }
     
