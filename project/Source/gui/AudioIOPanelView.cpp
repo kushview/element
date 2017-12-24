05e9d99c49857a6a1282ca2fb39f83664bf0038e
Michael Fisher <mfisher@kushview.net>
2017-12-24 02:43:35 -0500

    updated jucer metadata in these files

--- project/Source/gui/AudioIOPanelView.cpp
+++ project/Source/gui/AudioIOPanelView.cpp
@@ -7,7 +7,7 @@
   the "//[xyz]" and "//[/xyz]" sections will be retained when the file is loaded
   and re-saved.
 
-  Created with Projucer version: 5.1.2
+  Created with Projucer version: 5.2.0
 
   ------------------------------------------------------------------------------
 
