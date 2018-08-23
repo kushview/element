5cf853e606be1db8ca0b26cada76369e0dfa67ea
Michael Fisher <mfisher@kushview.net>
2018-08-23 10:44:28 -0400

    Update juce header

--- project/JuceLibraryCode/JuceHeader.h
+++ project/JuceLibraryCode/JuceHeader.h
@@ -48,6 +48,6 @@ namespace ProjectInfo
 {
     const char* const  projectName    = "Element";
     const char* const  versionString  = "0.19.2";
-    const int          versionNumber  = 0x1301;
+    const int          versionNumber  = 0x1302;
 }
 #endif
