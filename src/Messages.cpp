257f1cf8737860401aff2c5eafc27215d8e6aa22
Michael Fisher <mfisher@kushview.net>
2018-10-24 13:26:06 -0400

    Move action classes to Element namespace scope

--- src/Messages.cpp
+++ src/Messages.cpp
@@ -5,102 +5,103 @@
 
 namespace Element {
 
+class AddPluginAction : public UndoableAction
+{
+public:
+    AddPluginAction (AppController& _app, const AddPluginMessage& msg) 
+        : app (_app), graph (msg.graph), description (msg.description),
+            builder (msg.builder), verified (msg.verified) { }
+    ~AddPluginAction () noexcept { }
+
+    bool perform() override
+    {
+        addedNode = Node();
+
+        if (auto* ec = app.findChild<EngineController>())
+            if (graph.isGraph())
+                addedNode = ec->addPlugin (graph, description, builder, verified);
+
+        return addedNode.isValid();
+    }
+
+    bool undo() override
+    {
+        if (! addedNode.isValid())
+            return false;
+        if (auto* ec = app.findChild<EngineController>())
+            ec->removeNode (addedNode);
+        addedNode = Node();
+        return true;
+    }
+
+private:
+    AppController& app;
+    const Node graph;
+    const PluginDescription description;
+    const ConnectionBuilder builder;
+    const bool verified = true;
+    Node addedNode;
+};
+
+class RemoveNodeAction : public UndoableAction
+{
+public:
+    explicit RemoveNodeAction (AppController& a, const Node& node)
+        : app(a), nodeUuid (node.getUuid()), targetGraph (node.getParentGraph())
+    {
+        node.getArcs (arcs);
+        Node mutableNode (node);
+        mutableNode.savePluginState();
+        node.getRelativePosition (x, y);
+        nodeData = node.getValueTree().createCopy();
+        Node::sanitizeRuntimeProperties (nodeData);
+    }
+    
+    bool perform() override
+    {
+        auto& ec = *app.findChild<EngineController>();
+        ec.removeNode (nodeUuid);
+        return true;
+    }
+    
+    bool undo() override
+    {
+        auto& ec = *app.findChild<EngineController>();
+        bool handled = true;
+
+        const Node newNode (nodeData, false);
+        ec.addNode (newNode, targetGraph, builder);
+
+        for (const auto* arc : arcs)
+        {
+            ec.addConnection (arc->sourceNode, arc->sourcePort,
+                arc->destNode, arc->destPort, targetGraph);
+        }
+
+        return handled;
+    }
+
+private:
+    AppController& app;
+    ValueTree nodeData;
+    const Node targetGraph;
+    const Uuid nodeUuid;
+    ConnectionBuilder builder;
+    OwnedArray<Arc> arcs;
+    double x = 0.5;
+    double y = 0.5;
+    bool isDataValid() const {
+        return targetGraph.isGraph() && !nodeUuid.isNull() && nodeData.isValid();
+    }
+};
+
 void AddPluginMessage::createActions (AppController& app, OwnedArray<UndoableAction>& actions) const
 {
-    class AddPluginAction : public UndoableAction
-    {
-    public:
-        AddPluginAction (AppController& _app, const AddPluginMessage& msg) 
-            : app (_app), graph (msg.graph), description (msg.description),
-              builder (msg.builder), verified (msg.verified) { }
-        ~AddPluginAction () noexcept { }
-
-        bool perform() override
-        {
-            addedNode = Node();
-
-            if (auto* ec = app.findChild<EngineController>())
-                if (graph.isGraph())
-                    addedNode = ec->addPlugin (graph, description, builder, verified);
-
-            return addedNode.isValid();
-        }
-
-        bool undo() override
-        {
-            if (! addedNode.isValid())
-                return false;
-            if (auto* ec = app.findChild<EngineController>())
-                ec->removeNode (addedNode);
-            addedNode = Node();
-            return true;
-        }
-    
-    private:
-        AppController& app;
-        const Node graph;
-        const PluginDescription description;
-        const ConnectionBuilder builder;
-        const bool verified = true;
-        Node addedNode;
-    };
-
     actions.add (new AddPluginAction (app, *this));
 }
 
-void RemoveNodeMessage::createActions (AppController& app,
-                                       OwnedArray<UndoableAction>& actions) const
+void RemoveNodeMessage::createActions (AppController& app, OwnedArray<UndoableAction>& actions) const
 {
-    class RemoveNodeAction : public UndoableAction
-    {
-    public:
-        explicit RemoveNodeAction (AppController& a, const Node& node)
-            : app(a), nodeUuid (node.getUuid()), targetGraph (node.getParentGraph())
-        {
-            node.getArcs (arcs);
-            Node mutableNode (node);
-            mutableNode.savePluginState();
-            nodeData = node.getValueTree().createCopy();
-            Node::sanitizeRuntimeProperties (nodeData);
-        }
-        
-        bool perform() override
-        {
-            auto& ec = *app.findChild<EngineController>();
-            ec.removeNode (nodeUuid);
-            return true;
-        }
-        
-        bool undo() override
-        {
-            auto& ec = *app.findChild<EngineController>();
-            bool handled = true;
-
-            const Node newNode (nodeData, false);
-            ec.addNode (newNode, targetGraph, builder);
- 
-            for (const auto* arc : arcs)
-            {
-                ec.addConnection (arc->sourceNode, arc->sourcePort,
-                    arc->destNode, arc->destPort, targetGraph);
-            }
-
-            return handled;
-        }
-
-    private:
-        AppController& app;
-        ValueTree nodeData;
-        const Node targetGraph;
-        const Uuid nodeUuid;
-        ConnectionBuilder builder;
-        OwnedArray<Arc> arcs;
-
-        bool isDataValid() const {
-            return targetGraph.isGraph() && !nodeUuid.isNull() && nodeData.isValid();
-        }
-    };
-
     if (node.isValid())
         actions.add (new RemoveNodeAction (app, node));
     for (const auto& n : nodes)
