b03ed2821048ce9cb30b5c1b53915e43ae196e5b
Michael Fisher <mfisher@kushview.net>
2018-12-07 23:27:07 -0500

    Housekeeping

--- src/Settings.h
+++ src/Settings.h
@@ -16,7 +16,7 @@ class Settings :  public ApplicationProperties
 public:
     Settings();
     ~Settings();
-    
+
     static const char* checkForUpdatesKey;
     static const char* pluginListKey;
     static const char* pluginFormatsKey;
@@ -29,26 +29,26 @@ public:
     static const char* defaultNewSessionFile;
     static const char* generateMidiClockKey;
     static const char* sendMidiClockToInputKey;
-    
+
     XmlElement* getLastGraph() const;
     void setLastGraph (const ValueTree& data);
-    
+
     /** Returns true if updates shoul be checked for on launch */
     bool checkForUpdates() const;
-    
+
     /** Set if should check updates on start */
     void setCheckForUpdates (const bool shouldCheck);
-    
+
     /** Returns true if plugins should be scanned on startup */
     bool scanForPluginsOnStartup() const;
-    
+
     /** Set if plugins should be scanned during startup */
     void setScanForPluginsOnStartup (const bool shouldScan);
-    
+
     /** True if plugin windows should be made visible when added to a graph */
     bool showPluginWindowsWhenAdded() const;
     void setShowPluginWindowsWhenAdded (const bool);
-    
+
     /** True if the last used session should be opened on launch */
     bool openLastUsedSession() const;
     void setOpenLastUsedSession (const bool);
@@ -71,7 +71,7 @@ public:
 
     void setSendMidiClockToInput (const bool);
     bool sendMidiClockToInput() const;
-    
+
 private:
     PropertiesFile* getProps() const;
 };
