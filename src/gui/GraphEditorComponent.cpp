b37599e30226f6422bab91242ccc6d8cbec0aabc
Michael Fisher <mfisher@kushview.net>
2019-01-23 12:30:36 -0500

    Update copyright

--- src/gui/GraphEditorComponent.cpp
+++ src/gui/GraphEditorComponent.cpp
@@ -1,6 +1,6 @@
 /*
     GraphEditorComponent.cpp - This file is part of Element
-    Copyright (C) 2016  Kushview, LLC.  All rights reserved.
+    Copyright (C) 2016-2019  Kushview, LLC.  All rights reserved.
 */
 
 #include "ElementApp.h"
