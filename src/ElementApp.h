2a1f70ccecf329661c35961c8050d39549b60105
Michael Fisher <mfisher@kushview.net>
2019-03-01 06:26:51 -0500

    added collapsed tag

--- src/ElementApp.h
+++ src/ElementApp.h
@@ -27,6 +27,7 @@ namespace Tags
     const Identifier control            = "control";
     const Identifier controller         = "controller";
     const Identifier controllers        = "controllers";
+    const Identifier collapsed          = "collapsed";
     const Identifier enabled            = "enabled";
     const Identifier graph              = "graph";
     const Identifier graphs             = "graphs";
