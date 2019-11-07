ce7799b0475c321d14da4b6ebe01e0b9a739331c
Michael Fisher <mfisher@kushview.net>
2019-11-06 21:34:37 -0500

    Added simple lua test

--- tests/LuaTest.cpp
+++ tests/LuaTest.cpp
@@ -17,71 +17,39 @@
 */
 
 #include "Tests.h"
-#include <luapp/lua.hpp>
+#include <lua.hpp>
 
 using namespace Element;
 
-class LuaTest : public UnitTest
+class LuaTest : public UnitTestBase
 {
 public:
-    LuaTest() : UnitTest ("Lua", "lua") { }
+    LuaTest() : UnitTestBase ("Lua Basics", "Lua", "lua") {}
     virtual ~LuaTest() { }
 
     void runTest()
     {
-        
+        runSimpleScript();
     }
 
 private:
-    void testGmailFilter()
+    void runSimpleScript() 
     {
-        beginTest ("Gmail Extended Addresses");
-        expect (Util::isGmailExtended ("axetota@gmail.com") == false);
-        expect (Util::isGmailExtended ("axetota+extended@gmail.com") == true);
-        expect (Util::isGmailExtended ("axetota+@gmail.com") == true);
-        expect (Util::isGmailExtended ("axe.to.ta@gmail.com") == true);
-        expect (Util::isGmailExtended ("info+yadda@yahoo.com") == false);
-        expect (Util::isGmailExtended ("info.ya.dda@yahoo.com") == false);
+        // initialization
+        auto* lua = luaL_newstate();
+        luaL_openlibs (lua);
+
+        // execute script
+        String script = "print('Hello World!')";
+        int load_stat = luaL_loadbuffer (lua, script.toRawUTF8(), script.length(), script.toRawUTF8());
+        lua_pcall (lua, 0, 0, 0);
+
+        // cleanup
+        lua_close (lua);
     }
 
-    void testUuid()
-    {
-        beginTest ("UUID handling");
-    }
-
-    void testName()
-    {
-        beginTest ("getName() == 'Dummy'");
-        expect (getName() == "Dummy");
-    }
-
-    void testJavascript()
-    {
-        beginTest ("JS engine adds console.log(...)");
-        JavascriptEngine engine;
-        auto* object = new DynamicObject();
-        object->setMethod ("log", std::bind (&LuaTest::testLog, std::placeholders::_1));
-        engine.registerNativeObject (Identifier ("test"), object);
-        auto result = engine.execute (R"(
-            var console = { log: test.log };
-            console.log("Hello World", 123);
-        )");
-        expect (! result.failed());
-    }
-
-    static var testLog (const var::NativeFunctionArgs& args)
-    {
-        if (args.numArguments == 1)
-        {
-            Logger::writeToLog (args.arguments->toString());
-        }
-        else if (args.numArguments > 1)
-        {
-            for (int i = 0; i < args.numArguments; ++i)
-                Logger::writeToLog ((args.arguments + i)->toString());
-        }
-
-        return var::undefined();
+    void callCFunction() {
+        
     }
 };
 
