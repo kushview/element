9d195680ac27976f3ce92d42bdba70da3abc1a08
Michael Fisher <mfisher@kushview.net>
2019-12-23 07:24:01 -0500

    Formatting

--- tests/lua/rect.lua
+++ tests/lua/rect.lua
@@ -5,7 +5,7 @@ end
 function run()
     begintest ("rect")
     local r = kv.Rect (0, 5, 100, 100)
-    expect(r.w == 100, tostring(r))
+    expect (r.w == 100, tostring (r))
     expect (r.h == 100, tostring (r))
     expect (r.x == 0, tostring (r))
     expect (r.y == 5, tostring (r))
