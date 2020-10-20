49181b56a5e16d020309d22847008d3483480ee2
Michael Fisher <mfisher@kushview.net>
2020-10-20 13:02:30 -0400

    Playing with function based class

--- scripts/test.lua
+++ scripts/test.lua
@@ -1,3 +1,19 @@
-function descriptor()
-    return
+--- Test Script
+-- @name Test Script
+-- @author Michael Fisher
+-- @description A script for testing purposes
+
+local function MyObject()
+    local self = {}
+    function self.print()
+        print (self)
+    end
+    return self
 end
+
+local obj = MyObject()
+obj:print()
+obj.print()
+obj = MyObject()
+obj:print()
+obj.print()
