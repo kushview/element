6bfcfecfb4bb7df1403dc41c3d68117035585d82
Michael Fisher <mfisher@kushview.net>
2020-12-09 15:31:57 -0500

    Add headers to vscode.h

--- .vscode/vscode.h
+++ .vscode/vscode.h
@@ -1,6 +1,11 @@
 #define JUCE_AMALGAMATED_INCLUDE 1
 #include "JuceHeader.h"
+#include "ElementApp.h"
+#include "Globals.h"
+#include "lua.hpp"
+#include "sol/sol.hpp"
 using namespace juce;
+using namespace Element;
 
 namespace kv {
 using namespace juce;
