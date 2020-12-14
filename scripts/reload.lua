cf2a35a65ba43c50851c17cb79d023a9d4ccb54a
Michael Fisher <mfisher@kushview.net>
2020-12-14 10:30:40 -0500

    Upate reload script

--- scripts/reload.lua
+++ scripts/reload.lua
@@ -1,18 +1,23 @@
 
 local mods = {
     ["ui"]      = "element.ui",
-    ["widget"]  = "el.Widget",
-    ["window"]  = "el.Window",
-    ["object"]  = "kv.object"
+    ["widget"]  = "kv.Widget",
+    ["window"]  = "kv.Window",
+    ["object"]  = "kv.object",
+    ["script"]  = "el.script"
 }
 
-print ("Reloading modules...")
-for name, module in pairs (mods) do
-    print ("  " .. module)
-    _G[name] = nil
-    _ENV[name] = nil
-    package.loaded[module] = false
-    package.preload[module] = nil
-    -- require (module)
-    -- _ENV[name] = _G[name]
+local function reload()
+    print ("Reloading modules...")
+    for name, module in pairs (mods) do
+        print ("  " .. module)
+        _G[name] = nil
+        _ENV[name] = nil
+        package.loaded[module] = false
+        package.preload[module] = nil
+        _G['name'] = require (module)
+        _ENV[name] = _G[name]
+    end
 end
+
+return reload()
