0a41dc50477e00e8643d7c33618aad9b72a0ec69
Michael Fisher <mfisher@kushview.net>
2020-12-14 10:29:23 -0500

    Simplify script load/exec

--- libs/element/lua/el/script.lua
+++ libs/element/lua/el/script.lua
@@ -1,6 +1,19 @@
---- Script module
+--- Script module.
 -- @module el.script
 local M = {}
+
+--- Load a known script
+-- @function load
+-- @string path The script to run
+-- @tparam table env The environment to use or _ENV
+-- @tparam any ... Arguments passed to script
+-- @treturn table The script's descriptor table
+-- @usage script.load ('scriptname')
+function M.load (path, env)
+    local f, e = package.searchpath (path, package.spath)
+    if f then return loadfile (f, 'bt', env or _ENV) end
+    return nil, e
+end
 
 --- Run a known script
 -- @function exec
@@ -8,17 +21,11 @@ local M = {}
 -- @tparam table env The environment to use or _ENV
 -- @tparam any ... Arguments passed to script
 -- @treturn any Return value from script or no value
--- @usage element.script ('scriptname')
+-- @usage script.exec ('scriptname')
 function M.exec (path, env, ...)
-    local file, e1 = package.searchpath (path, package.spath)
-    if file then
-        local fn, e2 = loadfile (file, 'bt', env or _ENV)
-        if type(fn) == 'function' then
-            return fn(...)
-        end
-        return e2
-    end
-    return e1
+    local invoke, err = M.load (path, env, ...)
+    if err then return err end
+    return invoke (...)
 end
 
 return M
