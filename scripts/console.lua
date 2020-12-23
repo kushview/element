53ac4f87e41e2d3ec6165c8846f6699efdbc4cd5
Michael Fisher <mfisher@kushview.net>
2020-12-23 06:04:21 -0500

    Spice up console.lua init script

--- scripts/console.lua
+++ scripts/console.lua
@@ -1,2 +1,22 @@
-object = require ('kv.object')
-script = require ('el.script')
+--- Console init.
+-- Runs when the console is loaded in the GUI. This annonymous script sets global
+-- variables, so be careful if you use it directly.
+-- @script console
+
+io      = require ('io')
+object  = require ('kv.object')
+script  = require ('el.script')
+
+--- Built-in console helper table
+console = {
+    --- Print to stdout instead of the GUI display
+    log = function (...)
+        local out = ""
+        for i = 1, select ('#', ...) do
+            out = out .. tostring (select (i, ...)) .. "\t"
+        end
+        if string.len(out) > 0 then
+            io.stdout:write (out .. "\n")
+        end
+    end
+}
