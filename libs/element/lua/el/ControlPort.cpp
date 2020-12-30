7b1f09e807bd1d5d3db6cc2b6c42ac9491853e85
Michael Fisher <mfisher@kushview.net>
2020-12-30 17:30:12 -0500

    Add ControlPort (lua) placeholder

---
+++ libs/element/lua/el/ControlPort.cpp
@@ -0,0 +1,17 @@
+/// Global objects.
+// A collection of global objects 
+// @classmod el.Globals
+// @pragma nostrip
+
+// #include "lua-kv.hpp"
+// #include "engine/Parameter.h"
+
+
+
+// LUAMOD_API int luaopen_el_ControlPort (lua_State* L)
+// {
+//     using Element::Globals;
+//     sol::state_view lua (L);
+//     auto M = lua.create_table();
+//     M.new_usertype<Globals> ("Globals", sol::no_constructor);
+// }
