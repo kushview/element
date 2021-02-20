f81ced1fb7818bbc1a3d0cb6082cc70756942c13
Michael Fisher <mfisher@kushview.net>
2021-02-20 18:13:41 -0500

    Add tools to clean and resave jucer (IDE) projects.

---
+++ tools/resave-jucer.py
@@ -0,0 +1,19 @@
+from subprocess import call
+import sys
+
+PROJUCER_PROJECTS = '''
+    tools\jucer\Standalone\Element.jucer
+    tools\jucer\Standalone\Element.jucer
+    tools\jucer\Standalone\Element.jucer
+    tools\jucer\Standalone\Element.jucer'''.split()
+
+def projucer_exe():
+    if 'win' in sys.platform:
+        return "C:\\SDKs\\JUCE\\Projucer.exe"
+    elif 'darwin' in sys.platform:
+        return "/Applications/Projucer.app/Contents/MacOS/Projucer"
+    else:
+        return 'Projucer'
+
+for project in PROJUCER_PROJECTS:
+    call ([projucer_exe(), '--resave', project])
