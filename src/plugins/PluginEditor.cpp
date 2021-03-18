1e4c1a30a133fbfba2522ac4d968802d8c24b0f5
Michael Fisher <mfisher@kushview.net>
2021-03-18 16:34:34 -0400

    Privatize plugin editor code

--- src/plugins/PluginEditor.cpp
+++ src/plugins/PluginEditor.cpp
@@ -19,9 +19,11 @@
 
 #include "gui/GuiCommon.h"
 #include "gui/ContentComponent.h"
+#include "gui/LookAndFeel.h"
 #include "gui/PluginWindow.h"
 #include "plugins/PluginEditor.h"
 #include "plugins/PluginProcessor.h"
+#include "ElementApp.h"
 
 #define EL_PLUGIN_MIN_WIDTH         546
 #define EL_PLUGIN_MIN_HEIGHT        266
@@ -31,8 +33,216 @@
 namespace Element {
 using Element::LookAndFeel;
 
+//==============================================================================
+class PerformanceParameterSlider : public Slider,
+                                   private AudioProcessorParameter::Listener,
+                                   private Timer
+{
+public:
+    PerformanceParameterSlider (PerformanceParameter& p)
+        : param (p)
+    {
+        param.addListener (this);
+        setSliderStyle (Slider::RotaryHorizontalDrag);
+        setTextBoxStyle (Slider::NoTextBox, false, 10, 10);
+        setScrollWheelEnabled (false);
+
+        if (param.getNumSteps() != AudioProcessor::getDefaultNumParameterSteps())
+            setRange (0.0, 1.0, 1.0 / (param.getNumSteps() - 1.0));
+        else
+            setRange (0.0, 1.0);
+
+        handleNewParameterValue();
+        
+        onValueChange = [this]() { sliderValueChanged(); };
+        onDragStart   = [this]() { sliderStartedDragging(); };
+        onDragEnd     = [this]() { sliderStoppedDragging(); };
+        
+        updateEnablement();
+        updateToolTip();
+
+        param.onCleared = [this]()
+        {
+            updateEnablement();
+            updateToolTip();
+        };
+
+        startTimerHz (100);
+    }
+    
+    ~PerformanceParameterSlider()
+    {
+        param.onCleared = nullptr;
+        param.removeListener (this);
+    }
+    
+    void updateToolTip()
+    {
+        if (! param.haveNode())
+        {
+            setTooltip (param.getName (100));
+            return;
+        }
+        
+        String message = param.getNode().getName();
+        String paramName = param.getBoundParameterName();
+        if (paramName.isEmpty())
+            paramName = param.getName (100);
+        
+        if (message.isNotEmpty() && paramName.isNotEmpty())
+            message << " - " << paramName;
+        else if (paramName.isNotEmpty())
+            message = paramName;
+        
+        setTooltip (message);
+    }
+
+    void updateEnablement()
+    {
+        setEnabled (param.haveNode());
+    }
+
+    void mouseDown (const MouseEvent& ev) override;
+    
+private:
+    PerformanceParameter& param;
+    Atomic<int> parameterValueHasChanged { 0 };
+    bool isDragging = false;
+
+    //==============================================================================
+    void parameterValueChanged (int, float) override
+    {
+        parameterValueHasChanged = 1;
+    }
+
+    void parameterGestureChanged (int, bool) override {}
+
+    //==============================================================================
+    void timerCallback() override
+    {
+        if (parameterValueHasChanged.compareAndSetBool (0, 1))
+        {
+            handleNewParameterValue();
+            startTimerHz (50);
+        }
+        else
+        {
+            startTimer (jmin (250, getTimerInterval() + 10));
+        }
+    }
+
+    void updateTextDisplay()
+    {
+        jassert(MessageManager::getInstance()->isThisTheMessageThread());
+        // valueLabel.setText (getParameter().getCurrentValueAsText(), dontSendNotification);
+    }
+
+    void handleNewParameterValue()
+    {
+        jassert(MessageManager::getInstance()->isThisTheMessageThread());
+        if (! isDragging)
+        {
+            setValue (param.getValue(), dontSendNotification);
+            updateTextDisplay();
+        }
+    }
+
+    void sliderValueChanged()
+    {
+        auto newVal = (float) getValue();
+
+        if (param.getValue() != newVal)
+        {
+            if (! isDragging)
+                param.beginChangeGesture();
+
+            param.setValueNotifyingHost (newVal);
+            updateTextDisplay();
+
+            if (! isDragging)
+                param.endChangeGesture();
+        }
+    }
+
+    void sliderStartedDragging()
+    {
+        isDragging = true;
+        param.beginChangeGesture();
+    }
+
+    void sliderStoppedDragging()
+    {
+        isDragging = false;
+        param.endChangeGesture();
+    }
+};
+
+//==============================================================================
+class PerfSliders : public Component
+{
+public:
+    PerfSliders (PluginProcessor& processor)
+    {
+        for (auto* param : processor.getParameters())
+        {
+            if (auto* perf = dynamic_cast<PerformanceParameter*> (param))
+            {
+                auto* slider = sliders.add (new PerformanceParameterSlider (*perf));
+                addAndMakeVisible (slider);
+            }
+        }
+
+        setSize (500, 44);
+    }
+
+    void update()
+    {
+        for (auto* const slider : sliders)
+        {
+            slider->updateEnablement();
+            slider->updateToolTip();
+        }
+    }
+
+    void paint (Graphics& g) override
+    {
+        g.fillAll (Element::LookAndFeel::widgetBackgroundColor.darker (0.29));
+    }
+
+    void resized() override
+    {
+        int space = 12;
+        Rectangle<int> sb;
+        sb = getLocalBounds();
+        
+       #if 0
+        // right to left
+        sb.removeFromRight (space);
+        for (int i = sliders.size(); --i >= 0;)
+        {
+            auto* const slider = sliders [i];
+            slider->setBounds (sb.removeFromRight (44).reduced(4));
+            sb.removeFromRight (space);
+        }
+       #else
+        // left to right
+        sb.removeFromLeft (space);
+        for (int i = 0; i < sliders.size(); ++i)
+        {
+            auto* const slider = sliders [i];
+            slider->setBounds (sb.removeFromLeft (44).reduced (4));
+            sb.removeFromLeft (space);
+        }
+       #endif
+    }
+
+private:
+    OwnedArray<PerformanceParameterSlider> sliders;
+};
+
+//==============================================================================
 class PluginEditor::ParamTable : public TableListBox,
-                                                      public TableListBoxModel
+                                 public TableListBoxModel
 {
 public:
     ParamTable()
@@ -75,6 +285,7 @@ public:
 #endif
 };
 
+//==============================================================================
 void PerformanceParameterSlider::mouseDown (const MouseEvent& ev)
 {
     auto* const editor = findParentComponentOfClass<PluginEditor>();
@@ -92,6 +303,7 @@ void PerformanceParameterSlider::mouseDown (const MouseEvent& ev)
     return Slider::mouseDown (ev);
 }
 
+//==============================================================================
 class PluginEditor::ParamTableToggle : public Button
 {
 public:
@@ -109,9 +321,11 @@ protected:
     }
 };
 
+//==============================================================================
 PluginEditor::PluginEditor (PluginProcessor& plugin)
     : AudioProcessorEditor (&plugin), processor (plugin)
 {
+    setOpaque (true);
     paramTable.reset (new ParamTable());
     addAndMakeVisible (paramTable.get());
     paramToggle.reset (new ParamTableToggle());
@@ -134,45 +348,42 @@ PluginEditor::PluginEditor (PluginProcessor& plugin)
 
     auto* const app = processor.getAppController();
     auto* const gui = app->findChild<GuiController>();
+    
+    addAndMakeVisible (content = gui->getContentComponent());
+    jassert (content);
+    if (auto* cc = dynamic_cast<ContentComponent*> (content.getComponent()))
+        cc->setExtraView (new PerfSliders (processor));
 
+    setResizable (true, true);
+    getConstrainer()->setMinimumSize (EL_PLUGIN_MIN_WIDTH, EL_PLUGIN_MIN_HEIGHT);
+
+    const auto& bounds (processor.getEditorBounds());
+    if (bounds.getWidth() > 0 && bounds.getHeight() > 0)
     {
-        setOpaque (true);
-       
-        addAndMakeVisible (content = gui->getContentComponent());
-        jassert (content);
-        if (auto* cc = dynamic_cast<ContentComponent*> (content.getComponent()))
-            cc->setExtraView (new PerfSliders (processor));
+        setSize (jmax (EL_PLUGIN_MIN_WIDTH,  bounds.getWidth()), 
+                 jmax (EL_PLUGIN_MIN_HEIGHT, bounds.getHeight()));
+    }
+    else
+    {
+        setSize (EL_PLUGIN_DEFAULT_WIDTH, EL_PLUGIN_DEFAULT_HEIGHT);
+    }
 
-        setResizable (true, true);
-        getConstrainer()->setMinimumSize (EL_PLUGIN_MIN_WIDTH, EL_PLUGIN_MIN_HEIGHT);
-
-        const auto& bounds (processor.getEditorBounds());
-        if (bounds.getWidth() > 0 && bounds.getHeight() > 0)
+    setWantsPluginKeyboardFocus (processor.getEditorWantsKeyboard());
+    
+    gui->stabilizeContent();
+    
+    if (auto session = app->getWorld().getSession())
+    {
+        const auto graph (session->getActiveGraph());
+        if (graph.isGraph())
         {
-            setSize (jmax (EL_PLUGIN_MIN_WIDTH,  bounds.getWidth()), 
-                     jmax (EL_PLUGIN_MIN_HEIGHT, bounds.getHeight()));
-        }
-        else
-        {
-            setSize (EL_PLUGIN_DEFAULT_WIDTH, EL_PLUGIN_DEFAULT_HEIGHT);
-        }
-
-        setWantsPluginKeyboardFocus (processor.getEditorWantsKeyboard());
-        
-        gui->stabilizeContent();
-        
-        if (auto session = app->getWorld().getSession())
-        {
-            const auto graph (session->getActiveGraph());
-            if (graph.isGraph())
-            {
-                gui->showPluginWindowsFor (graph);
-                for (int w = 0; w < gui->getNumPluginWindows(); ++w)
-                    if (auto* window = gui->getPluginWindow (w))
-                        window->toFront (false);
-            }
+            gui->showPluginWindowsFor (graph);
+            for (int w = 0; w < gui->getNumPluginWindows(); ++w)
+                if (auto* window = gui->getPluginWindow (w))
+                    window->toFront (false);
         }
     }
+
 
     perfParamChangedConnection = processor.onPerfParamsChanged.connect (std::bind (
         &PluginEditor::updatePerformanceParamEnablements, this));
@@ -193,11 +404,7 @@ PluginEditor::~PluginEditor()
     }
 }
 
-void PluginEditor::handleAsyncUpdate()
-{
-    // noop
-}
-
+//==============================================================================
 Element::ContentComponent* PluginEditor::getContentComponent()
 {
     if (nullptr == content)
@@ -205,7 +412,56 @@ Element::ContentComponent* PluginEditor::getContentComponent()
             if (auto* gui = app->findChild<GuiController>())
                 content = gui->getContentComponent();
     return nullptr != content ? dynamic_cast<Element::ContentComponent*> (content.getComponent())
-                              : 0;
+                              : nullptr;
+}
+
+//==============================================================================
+void PluginEditor::setWantsPluginKeyboardFocus (bool focus)
+{
+    if (focus == getWantsKeyboardFocus())
+        return;
+
+    setWantsKeyboardFocus (focus);
+    processor.setEditorWantsKeyboard (focus);
+    
+    if (getWantsKeyboardFocus())
+    {
+        addKeyListener (this);
+    }
+    else
+    {
+        removeKeyListener (this);
+    }
+}
+
+bool PluginEditor::getWantsPluginKeyboardFocus() const
+{
+    return getWantsKeyboardFocus();
+}
+
+//==============================================================================
+int PluginEditor::getLatencySamples() const             { return processor.getLatencySamples(); }
+void PluginEditor::setReportZeroLatency (bool force)    { processor.setForceZeroLatency (force); }
+bool PluginEditor::isReportingZeroLatency() const       { return processor.isForcingZeroLatency(); }
+
+//==============================================================================
+void PluginEditor::paint (Graphics& g)
+{
+    g.fillAll (Element::LookAndFeel::widgetBackgroundColor.darker (0.29));
+    if (! content)
+    {
+        g.setColour (Element::LookAndFeel::textColor);
+        g.drawFittedText ("Unauthorized: Please activate your license in the application.",
+                           0, 0, getWidth(), getHeight(), Justification::centred, 2);
+    }
+}
+
+void PluginEditor::resized()
+{
+    auto bounds (getLocalBounds());
+    processor.setEditorBounds (bounds);
+    if (content)
+        content->setBounds (bounds);
 }
 
 bool PluginEditor::keyPressed (const KeyPress &key, Component *originatingComponent)
@@ -225,23 +481,12 @@ bool PluginEditor::keyPressed (const KeyPress &key, Component *originatingCompon
     return false;
 }
 
-void PluginEditor::paint (Graphics& g)
+//==============================================================================
+void PluginEditor::updatePerformanceParamEnablements()
 {
-    g.fillAll (Element::LookAndFeel::widgetBackgroundColor.darker (0.29));
-    if (! content)
-    {
-        g.setColour (Element::LookAndFeel::textColor);
-        g.drawFittedText ("Unauthorized: Please activate your license in the application.",
-                           0, 0, getWidth(), getHeight(), Justification::centred, 2);
-    }
-}
-
-void PluginEditor::resized()
-{
-    auto bounds (getLocalBounds());
-    processor.setEditorBounds (bounds);
-    if (content)
-        content->setBounds (bounds);
+    if (auto* cc = dynamic_cast<ContentComponent*> (content.getComponent()))
+        if (auto* ps = dynamic_cast<PerfSliders*> (cc->getExtraView()))
+            ps->update();
 }
 
 }
--- src/plugins/PluginEditor.h
+++ src/plugins/PluginEditor.h
@@ -19,276 +19,42 @@
 
 #pragma once
 
-#include "ElementApp.h"
-#include "PluginProcessor.h"
-#include "gui/ContentComponent.h"
-#include "gui/LookAndFeel.h"
+#include "JuceHeader.h"
 #include "Signals.h"
 
 namespace Element {
 
-class PerformanceParameterSlider : public Slider,
-                                   private AudioProcessorParameter::Listener,
-                                   private Timer
-{
-public:
-    PerformanceParameterSlider (PerformanceParameter& p)
-        : param (p)
-    {
-        param.addListener (this);
-        setSliderStyle (Slider::RotaryHorizontalDrag);
-        setTextBoxStyle (Slider::NoTextBox, false, 10, 10);
-        setScrollWheelEnabled (false);
+class ContentComponent;
+class PluginProcessor;
 
-        if (param.getNumSteps() != AudioProcessor::getDefaultNumParameterSteps())
-            setRange (0.0, 1.0, 1.0 / (param.getNumSteps() - 1.0));
-        else
-            setRange (0.0, 1.0);
-
-        handleNewParameterValue();
-        
-        onValueChange = [this]() { sliderValueChanged(); };
-        onDragStart   = [this]() { sliderStartedDragging(); };
-        onDragEnd     = [this]() { sliderStoppedDragging(); };
-        
-        updateEnablement();
-        updateToolTip();
-
-        param.onCleared = [this]()
-        {
-            updateEnablement();
-            updateToolTip();
-        };
-
-        startTimerHz (100);
-    }
-    
-    ~PerformanceParameterSlider()
-    {
-        param.onCleared = nullptr;
-        param.removeListener (this);
-    }
-    
-    void updateToolTip()
-    {
-        if (! param.haveNode())
-        {
-            setTooltip (param.getName (100));
-            return;
-        }
-        
-        String message = param.getNode().getName();
-        String paramName = param.getBoundParameterName();
-        if (paramName.isEmpty())
-            paramName = param.getName (100);
-        
-        if (message.isNotEmpty() && paramName.isNotEmpty())
-            message << " - " << paramName;
-        else if (paramName.isNotEmpty())
-            message = paramName;
-        
-        setTooltip (message);
-    }
-
-    void updateEnablement()
-    {
-        setEnabled (param.haveNode());
-    }
-
-    void mouseDown (const MouseEvent& ev) override;
-    
-private:
-    PerformanceParameter& param;
-    Atomic<int> parameterValueHasChanged { 0 };
-    bool isDragging = false;
-
-    //==============================================================================
-    void parameterValueChanged (int, float) override
-    {
-        parameterValueHasChanged = 1;
-    }
-
-    void parameterGestureChanged (int, bool) override {}
-
-    //==============================================================================
-    void timerCallback() override
-    {
-        if (parameterValueHasChanged.compareAndSetBool (0, 1))
-        {
-            handleNewParameterValue();
-            startTimerHz (50);
-        }
-        else
-        {
-            startTimer (jmin (250, getTimerInterval() + 10));
-        }
-    }
-
-    void updateTextDisplay()
-    {
-        jassert(MessageManager::getInstance()->isThisTheMessageThread());
-        // valueLabel.setText (getParameter().getCurrentValueAsText(), dontSendNotification);
-    }
-
-    void handleNewParameterValue()
-    {
-        jassert(MessageManager::getInstance()->isThisTheMessageThread());
-        if (! isDragging)
-        {
-            setValue (param.getValue(), dontSendNotification);
-            updateTextDisplay();
-        }
-    }
-
-    void sliderValueChanged()
-    {
-        auto newVal = (float) getValue();
-
-        if (param.getValue() != newVal)
-        {
-            if (! isDragging)
-                param.beginChangeGesture();
-
-            param.setValueNotifyingHost (newVal);
-            updateTextDisplay();
-
-            if (! isDragging)
-                param.endChangeGesture();
-        }
-    }
-
-    void sliderStartedDragging()
-    {
-        isDragging = true;
-        param.beginChangeGesture();
-    }
-
-    void sliderStoppedDragging()
-    {
-        isDragging = false;
-        param.endChangeGesture();
-    }
-};
-
-class PerfSliders : public Component
-{
-public:
-    PerfSliders (PluginProcessor& processor)
-    {
-        for (auto* param : processor.getParameters())
-        {
-            if (auto* perf = dynamic_cast<PerformanceParameter*> (param))
-            {
-                auto* slider = sliders.add (new PerformanceParameterSlider (*perf));
-                addAndMakeVisible (slider);
-            }
-        }
-
-        setSize (500, 44);
-    }
-
-    void update()
-    {
-        for (auto* const slider : sliders)
-        {
-            slider->updateEnablement();
-            slider->updateToolTip();
-        }
-    }
-
-    void paint (Graphics& g) override
-    {
-        g.fillAll (Element::LookAndFeel::widgetBackgroundColor.darker (0.29));
-    }
-
-    void resized() override
-    {
-        int space = 12;
-        Rectangle<int> sb;
-        sb = getLocalBounds();
-        
-       #if 0
-        // right to left
-        sb.removeFromRight (space);
-        for (int i = sliders.size(); --i >= 0;)
-        {
-            auto* const slider = sliders [i];
-            slider->setBounds (sb.removeFromRight (44).reduced(4));
-            sb.removeFromRight (space);
-        }
-       #else
-        // left to right
-        sb.removeFromLeft (space);
-        for (int i = 0; i < sliders.size(); ++i)
-        {
-            auto* const slider = sliders [i];
-            slider->setBounds (sb.removeFromLeft (44).reduced (4));
-            sb.removeFromLeft (space);
-        }
-       #endif
-    }
-
-private:
-    OwnedArray<PerformanceParameterSlider> sliders;
-};
-
-class PluginEditor  : public AudioProcessorEditor,
-                                           public KeyListener,
-                                           public AsyncUpdater
+/** The audio processor editor used for Element plugins */
+class PluginEditor : public AudioProcessorEditor,
+                     public KeyListener
 {
 public:
     PluginEditor (PluginProcessor&);
     ~PluginEditor();
 
-    void paint (Graphics&) override;
-    void resized() override;
+    //==========================================================================
+    ContentComponent* getContentComponent();
 
-    bool keyPressed (const KeyPress &key, Component *originatingComponent) override;
-    bool keyStateChanged (bool isKeyDown, Component *originatingComponent) override { return true; }
-    
-    Element::ContentComponent* getContentComponent();
-    
-    void handleAsyncUpdate() override;
+    //==========================================================================
     PluginProcessor& getProcessor() { return processor; }
 
-    void updatePerformanceParamEnablements()
-    {
-        if (auto* cc = dynamic_cast<ContentComponent*> (content.getComponent()))
-            if (auto* ps = dynamic_cast<PerfSliders*> (cc->getExtraView()))
-                ps->update();
-    }
+    //==========================================================================
+    void setWantsPluginKeyboardFocus (bool focus);
+    bool getWantsPluginKeyboardFocus() const;
 
-    void setWantsPluginKeyboardFocus (bool focus)
-    {
-        if (focus == getWantsKeyboardFocus())
-            return;
+    //==========================================================================
+    int getLatencySamples() const;
+    void setReportZeroLatency (bool force);
+    bool isReportingZeroLatency() const;
 
-        setWantsKeyboardFocus (focus);
-        processor.setEditorWantsKeyboard (focus);
-        
-        if (getWantsKeyboardFocus())
-        {
-            addKeyListener (this);
-        }
-        else
-        {
-            removeKeyListener (this);
-        }
-    }
-
-    bool getWantsPluginKeyboardFocus() const
-    {
-        return getWantsKeyboardFocus();
-    }
-
-    int getLatencySamples() const { return processor.getLatencySamples(); }
-
-    void setReportZeroLatency (bool force)
-    {
-        processor.setForceZeroLatency (force);
-    }
-
-    bool isReportingZeroLatency() const { return processor.isForcingZeroLatency(); }
+    //==========================================================================
+    void paint (Graphics&) override;
+    void resized() override;
+    bool keyPressed (const KeyPress &key, Component *originatingComponent) override;
+    bool keyStateChanged (bool isKeyDown, Component *originatingComponent) override { return true; }
 
 private:
     PluginProcessor& processor;
@@ -299,6 +65,8 @@ private:
     bool paramTableVisible = false;
     class ParamTable; std::unique_ptr<ParamTable> paramTable;
     class ParamTableToggle; std::unique_ptr<ParamTableToggle> paramToggle;
+
+    void updatePerformanceParamEnablements();
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (PluginEditor)
 };
