01efec3d410202e6756ec4e8bedebddc40d857b2
Michael Fisher <mfisher@kushview.net>
2021-03-18 03:08:04 -0400

    Add new Instantiate methods to plugin projects

---
+++ tools/jucer/ElementFX/Source/ElementFX.cpp
@@ -0,0 +1,27 @@
+/*
+    This file is part of Element
+    Copyright (C) 2021  Kushview, LLC.  All rights reserved.
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+#include "plugins/PluginProcessor.h"
+#include "../../../libs/compat/BinaryData.cpp"
+
+juce::AudioProcessor* JUCE_CALLTYPE createPluginFilter()
+{
+    return new Element::PluginProcessor (
+        Element::PluginProcessor::Effect, 16);
+}
