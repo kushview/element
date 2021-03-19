fbee2136f7c1fab438865e97a837c1bd5efb0c9f
Michael Fisher <mfisher@kushview.net>
2021-03-19 00:01:21 -0400

    Rename option to no-local-lua in run.py

--- tools/run.py
+++ tools/run.py
@@ -3,14 +3,14 @@ from subprocess import call
 from optparse import OptionParser
 import os
 
-def set_intree_lua_paths():
+def set_local_lua_paths():
     os.environ ['LUA_PATH']             = "libs/lua-kv/src/?.lua;libs/element/lua/?.lua"
-    os.environ ['LUA_CPATH']            = "build/lib/lua/?.lua"
+    os.environ ['LUA_CPATH']            = "build/lib/lua/?.so"
     os.environ ['ELEMENT_SCRIPTS_PATH'] = "scripts/?.lua"
 
 def options():
     parser = OptionParser()
-    parser.add_option ("--system-lua", action="store_true", dest="syslua", default=False,
+    parser.add_option ("--no-local-lua", action="store_false", dest="local_lua", default=True,
         help="Load system lua modules and scripts instead of in tree")
 
     (opts, args) = parser.parse_args()
@@ -18,8 +18,8 @@ def options():
 
 def main():
     opts = options()
-    if not opts.syslua:
-        set_intree_lua_paths()
+    if opts.local_lua:
+        set_local_lua_paths()
 
     cmd = ['build/bin/element']
     call (cmd)
