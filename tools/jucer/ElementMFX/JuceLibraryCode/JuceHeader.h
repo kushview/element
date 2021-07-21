82febc3dde01c583dbf5ef855ccafea89b30cafc
Michael Fisher <mfisher@kushview.net>
2021-07-21 13:10:14 -0400

    Set version no. 0.47.0

--- tools/jucer/ElementMFX/JuceLibraryCode/JuceHeader.h
+++ tools/jucer/ElementMFX/JuceLibraryCode/JuceHeader.h
@@ -55,7 +55,7 @@ namespace ProjectInfo
 {
     const char* const  projectName    = "Element MFX";
     const char* const  companyName    = "Kushview";
-    const char* const  versionString  = "1.46.2";
-    const int          versionNumber  = 0x12e02;
+    const char* const  versionString  = "1.47.0";
+    const int          versionNumber  = 0x12f00;
 }
 #endif
