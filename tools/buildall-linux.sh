0527b992dbe5d6891a6b7adde142d1e9fe4847ef
Michael Fisher <mfisher@kushview.net>
2021-10-06 13:35:56 -0400

    Update linux buildall script

--- tools/buildall-linux.sh
+++ tools/buildall-linux.sh
@@ -11,7 +11,7 @@ here="`pwd`"
 export CFLAGS="-DEL_APPIMAGE=1"
 export CXXFLAGS="-DEL_APPIMAGE=1"
 ./waf configure --prefix=/usr $@
-./waf clean build docs --progress
+./waf resave clean build docs --progress
 
 cd "${here}/tools/jucer/Element/Builds/LinuxMakefile"
 make clean && rm -rf build && make -j4 CONFIG=Release
