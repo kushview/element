a432127341588a0c6a0db4466110d299efc77b00
Michael Fisher <mfisher@kushview.net>
2022-03-19 13:01:46 -0400

    Better std::filesystem inclusion

--- libs/element/src/filesystem.hpp
+++ libs/element/src/filesystem.hpp
@@ -1,22 +1,67 @@
-#pragma once
+// see: https://stackoverflow.com/questions/53365538/how-to-determine-whether-to-use-filesystem-or-experimental-filesystem
+// We haven't checked which filesystem to include yet
 
-// clang-format off
+// Check for feature test macro for <filesystem>
+#if defined(__cpp_lib_filesystem)
+    #define EL_FILESYSTEM_EXPERIMENTAL 0
 
-#if __linux__ || defined(__MINGW32__)
- #if __GNUC__ > 7
-  #include <filesystem>
- #else
-  #include <experimental/filesystem>
-  namespace std { namespace filesystem = experimental::filesystem; }
- #endif
+// Check for feature test macro for <experimental/filesystem>
+#elif defined(__cpp_lib_experimental_filesystem)
+    #define EL_FILESYSTEM_EXPERIMENTAL 1
 
-#elif defined(__APPLE__)
- #if __MAC_OS_X_VERSION_MIN_REQUIRED > MAC_OS_X_VERSION_10_14
-  #include <filesystem>
- #else
-  #include <experimental/filesystem>
-  namespace std { namespace filesystem = experimental::filesystem; }
- #endif
+// We can't check if headers exist...
+// Let's assume experimental to be safe
+#elif ! defined(__has_include)
+    #define EL_FILESYSTEM_EXPERIMENTAL 1
+
+// Check if the header "<filesystem>" exists
+#elif __has_include(<filesystem>)
+
+    // If we're compiling on Visual Studio and are not compiling with C++17, we need to use experimental
+    #ifdef _MSC_VER
+
+        // Check and include header that defines "_HAS_CXX17"
+        #if __has_include(<yvals_core.h>)
+            #include <yvals_core.h>
+
+            // Check for enabled C++17 support
+            #if defined(_HAS_CXX17) && _HAS_CXX17
+                // We're using C++17, so let's use the normal version
+                #define EL_FILESYSTEM_EXPERIMENTAL 0
+            #endif
+        #endif
+
+        // If the marco isn't defined yet, that means any of the other VS specific checks failed, so we need to use experimental
+        #ifndef EL_FILESYSTEM_EXPERIMENTAL
+            #define EL_FILESYSTEM_EXPERIMENTAL 1
+        #endif
+
+    // Not on Visual Studio. Let's use the normal version
+    #else // #ifdef _MSC_VER
+        #define EL_FILESYSTEM_EXPERIMENTAL 0
+    #endif
+
+// Check if the header "<filesystem>" exists
+#elif __has_include(<experimental/filesystem>)
+    #define EL_FILESYSTEM_EXPERIMENTAL 1
+
+// Fail if neither header is available with a nice error message
+#else
+    #error Could not find system header "<filesystem>" or "<experimental/filesystem>"
 #endif
 
-// clang-format on
+// We priously determined that we need the exprimental version
+#if EL_FILESYSTEM_EXPERIMENTAL
+    // Include it
+    #include <experimental/filesystem>
+
+// We need the alias from std::experimental::filesystem to std::filesystem
+namespace std {
+namespace filesystem = experimental::filesystem;
+}
+
+// We have a decent compiler and can use the normal version
+#else
+    // Include it
+    #include <filesystem>
+#endif
