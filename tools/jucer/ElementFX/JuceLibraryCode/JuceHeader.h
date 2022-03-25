a0115e1b8c7169e906c71cce22a480820c426cd5
Michael Fisher <mfisher@kushview.net>
2022-03-25 16:31:00 -0400

    Version bump 0.46.5

--- tools/jucer/ElementFX/JuceLibraryCode/JuceHeader.h
+++ tools/jucer/ElementFX/JuceLibraryCode/JuceHeader.h
@@ -56,7 +56,7 @@ namespace ProjectInfo
 {
     const char* const  projectName    = "Element FX";
     const char* const  companyName    = "Kushview";
-    const char* const  versionString  = "1.46.4";
-    const int          versionNumber  = 0x12e04;
+    const char* const  versionString  = "1.46.5";
+    const int          versionNumber  = 0x12e05;
 }
 #endif
