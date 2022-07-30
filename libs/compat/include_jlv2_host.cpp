ec90647394afba5abb237630c1daa058888f2b9f
Michael Fisher <mfisher@kushview.net>
2022-07-30 14:27:03 -0400

    Remove unused code file

--- libs/compat/include_jlv2_host.cpp
+++
@@ -1,9 +0,0 @@
-/*
-
-    IMPORTANT! This file is auto-generated each time you save your
-    project - if you alter its contents, your changes may be overwritten!
-
-*/
-
-#include "AppConfig.h"
-#include <jlv2_host/jlv2_host.cpp>
