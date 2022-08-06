499b06c3cf6cd8db7aafdd634820b5e01ffa1884
Michael Fisher <mfisher@kushview.net>
2022-08-06 13:23:48 -0400

    Switching docker deployment to use meson

--- tools/lindeploy/appimage.sh
+++ tools/lindeploy/appimage.sh
@@ -3,23 +3,21 @@
 
 set -ex
 
-appdir="build/AppDir"
-./waf install --destdir="${appdir}"
+appdir="AppDir"
+rm -rf "build-native/$appdir"
+mkdir -p "build-native/$appdir"
+meson install --destdir="${appdir}" -C build-native
 
 if [ -z "${VERSION}" ]; then
     export VERSION="$(python waf version --revision)"
 fi
 
+cd build-native
 export LD_LIBRARY_PATH="build/lib:${LD_LIBRARY_PATH}"
 linuxdeploy --appimage-extract-and-run \
     --appdir ${appdir} --output appimage \
-    --desktop-file="build/share/applications/net.kushview.element.desktop" \
-    --icon-file="build/share/icons/hicolor/16x16/apps/net.kushview.element.png" \
-    --icon-file="build/share/icons/hicolor/32x32/apps/net.kushview.element.png" \
-    --icon-file="build/share/icons/hicolor/64x64/apps/net.kushview.element.png" \
-    --icon-file="build/share/icons/hicolor/128x128/apps/net.kushview.element.png" \
-    --icon-file="build/share/icons/hicolor/256x256/apps/net.kushview.element.png" \
-    --icon-file="build/share/icons/hicolor/512x512/apps/net.kushview.element.png" \
-    --executable="build/bin/element"
-mv *.AppImage build/
+    --desktop-file="`find "$appdir" -name "net.kushview.element.desktop"`" \
+    --icon-file="../data/ElementIcon_256x256.png" \
+    --icon-filename="net.kushview.element" \
+    --executable="./element"
 rm -rf "${appdir}"
--- tools/lindeploy/entrypoint.sh
+++ tools/lindeploy/entrypoint.sh
@@ -3,17 +3,24 @@
 
 set -e
 
+export BOOST_ROOT="/depends/x86_64-pc-linux-gnu/include"
 export CFLAGS="-DEL_APPIMAGE=1"
 export CXXFLAGS="-DEL_APPIMAGE=1"
-./waf configure --prefix=/usr \
-    --depends="/depends/x86_64-pc-linux-gnu" \
-    --depends-allow-system \
-    --with-vstsdk24="/SDKs/vstsdk2.4" \
-    CC="clang" CXX="clang++" \
-    ${WAF_CONFIGURE_OPTIONS}
-./waf build ${WAF_BUILD_OPTIONS}
-./waf docs
+rm -rf build-docker
+meson setup -Dbuildtype=release \
+    --native-file="tools/machine/linux.ini" \
+    build-docker
+meson compile -C build-docker -v -j1
 
-export LD_LIBRARY_PATH="/depends/x86_64-pc-linux-gnu/lib"
-sh tools/lindeploy/appimage.sh
-cp -fv build/*.AppImage /dist/
+# ./waf configure --prefix=/usr \
+#     --depends="/depends/x86_64-pc-linux-gnu" \
+#     --depends-allow-system \
+#     --with-vstsdk24="/SDKs/vstsdk2.4" \
+#     CC="clang" CXX="clang++" \
+#     ${WAF_CONFIGURE_OPTIONS}
+# ./waf build ${WAF_BUILD_OPTIONS}
+# ./waf docs
+
+# export LD_LIBRARY_PATH="/depends/x86_64-pc-linux-gnu/lib"
+# sh tools/lindeploy/appimage.sh
+# cp -fv build/*.AppImage /dist/
