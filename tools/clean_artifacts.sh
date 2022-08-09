32f585339d06a1e0284bcd54adca3bf4273463c9
Michael Fisher <mfisher@kushview.net>
2022-08-09 13:24:58 -0400

    Clean out useless tools

--- tools/clean_artifacts.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-find build -type f -name "*.a" -delete
-find build -type f -name "*.dll" -delete
-find build -type f -name "*.exe" -delete
-find build -type f -name "*.so"  -delete
-find build -type f -name "element*"  -delete
--- tools/element.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-set OPATH="%PATH%"
-set PATH="%~dp0bin;%~dp0lib;%PATH%"
-set LUA_PATH=%~dp0lua\?.lua
-"%~dp0bin\element.exe" %*
-set PATH="%OPATH%"
-set OPATH=""
