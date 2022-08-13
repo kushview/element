0be4366832651fc5140f439189ae47c47de3ef1f
Michael Fisher <mfisher@kushview.net>
2022-08-13 17:50:13 -0400

    Remove old file

--- tools/lua.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-export LD_LIBRARY_PATH="`pwd`/build/lib"
-export LUA_PATH="libs/lua-kv/src/?.lua;libs/lua/?.lua"
-export LUA_CPATH="build/lib/lua/?.lua"
-export ELEMENT_SCRIPTS_PATH="scripts/?.lua"
-build/bin/lua-el $@
-exit $?
