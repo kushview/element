b140be6adeecdbddd3ad98a995cf649142a94005
Michael Fisher <mfisher@kushview.net>
2022-08-25 09:53:38 -0400

    makepkg.sh: add method to codesign products and installer

--- tools/macdeploy/makepkg.sh
+++ tools/macdeploy/makepkg.sh
@@ -4,18 +4,31 @@ set -e
 rm -rf build/stage build/dist build/dmg dist
 meson install -C build --destdir="."
 
-python tools/macdeploy/appbundle.py build/stage/lib/vst/KV-Element.vst -verbose 0 -distdir="build/dist" -no-clean
-python tools/macdeploy/appbundle.py build/stage/lib/vst/KV-Element-FX.vst -verbose 0 -distdir="build/dist" -no-clean
+appbundle_opts="-verbose 1 -distdir=build/dist -no-clean"
+if [ -n "${CODESIGN_IDENTITY}" ]; then
+    export CODESIGNARGS="--sign \"${CODESIGN_IDENTITY}\" --timestamp --deep"
+    appbundle_opts+=" -sign"
+fi
 
-python tools/macdeploy/appbundle.py build/stage/lib/vst3/KV-Element.vst3 -verbose 0  -distdir="build/dist" -no-clean
-python tools/macdeploy/appbundle.py build/stage/lib/vst3/KV-Element-FX.vst3 -verbose 0  -distdir="build/dist" -no-clean
+python tools/macdeploy/appbundle.py build/stage/lib/vst/KV-Element.vst ${appbundle_opts}
+python tools/macdeploy/appbundle.py build/stage/lib/vst/KV-Element-FX.vst ${appbundle_opts}
 
-python tools/macdeploy/appbundle.py build/stage/lib/au/KV-Element.component -verbose 0  -distdir="build/dist" -no-clean
-python tools/macdeploy/appbundle.py build/stage/lib/au/KV-Element-FX.component -verbose 0  -distdir="build/dist" -no-clean
-python tools/macdeploy/appbundle.py build/stage/lib/au/KV-Element-MFX.component -verbose 0  -distdir="build/dist" -no-clean
+python tools/macdeploy/appbundle.py build/stage/lib/vst3/KV-Element.vst3 ${appbundle_opts}
+python tools/macdeploy/appbundle.py build/stage/lib/vst3/KV-Element-FX.vst3 ${appbundle_opts}
 
-python tools/macdeploy/appbundle.py build/stage/Element.app -verbose 0  -distdir="build/dist" -no-clean
+python tools/macdeploy/appbundle.py build/stage/lib/au/KV-Element.component ${appbundle_opts}
+python tools/macdeploy/appbundle.py build/stage/lib/au/KV-Element-FX.component ${appbundle_opts}
+python tools/macdeploy/appbundle.py build/stage/lib/au/KV-Element-MFX.component ${appbundle_opts}
 
-/usr/local/bin/packagesbuild -v --build-folder="`pwd`/build/dmg" tools/macdeploy/element.pkgproj
+python tools/macdeploy/appbundle.py build/stage/Element.app ${appbundle_opts}
+
+packages_opts=""
+if [ -z "INSTALLER_IDENTITY" ]; then
+    packages_opts+=" --identity=\"$INSTALLER_IDENTITY\""
+fi
+
+/usr/local/bin/packagesbuild -v --build-folder="`pwd`/build/dmg" \
+    ${packages_opts} tools/macdeploy/element.pkgproj
+
 dmgname=$(python tools/version.py --before="element-osx-" --revision --after=".dmg")
 hdiutil create -srcfolder "build/dmg" -format "UDZO" -volname "Element" -ov "build/$dmgname"
