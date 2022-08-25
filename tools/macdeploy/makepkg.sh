a4e48e7afaa50232567fd2e724121b6f473541f8
Michael Fisher <mfisher@kushview.net>
2022-08-25 10:53:03 -0400

    makepkg.sh: conditional signing args

--- tools/macdeploy/makepkg.sh
+++ tools/macdeploy/makepkg.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
-set -e
-rm -rf build/stage build/dist build/dmg dist
-meson install -C build --destdir="."
-
 appbundle_opts="-verbose 1 -distdir=build/dist -no-clean"
 if [ -n "${CODESIGN_IDENTITY}" ]; then
     export CODESIGNARGS="--sign \"${CODESIGN_IDENTITY}\" --timestamp --deep"
     appbundle_opts+=" -sign"
 fi
+packages_opts=""
+if [ -n "${INSTALLER_IDENTITY}" ]; then
+    packages_opts+=" --identity=\"$INSTALLER_IDENTITY\""
+fi
+
+dmgname=$(python tools/version.py --before="element-osx-" --revision --after=".dmg")
+
+set -e
+rm -rf build/stage build/dist build/dmg dist
+meson install -C build --destdir="."
 
 python tools/macdeploy/appbundle.py build/stage/lib/vst/KV-Element.vst ${appbundle_opts}
 python tools/macdeploy/appbundle.py build/stage/lib/vst/KV-Element-FX.vst ${appbundle_opts}
@@ -22,13 +28,7 @@ python tools/macdeploy/appbundle.py build/stage/lib/au/KV-Element-MFX.component 
 
 python tools/macdeploy/appbundle.py build/stage/Element.app ${appbundle_opts}
 
-packages_opts=""
-if [ -z "INSTALLER_IDENTITY" ]; then
-    packages_opts+=" --identity=\"$INSTALLER_IDENTITY\""
-fi
-
 /usr/local/bin/packagesbuild -v --build-folder="`pwd`/build/dmg" \
     ${packages_opts} tools/macdeploy/element.pkgproj
 
-dmgname=$(python tools/version.py --before="element-osx-" --revision --after=".dmg")
 hdiutil create -srcfolder "build/dmg" -format "UDZO" -volname "Element" -ov "build/$dmgname"
