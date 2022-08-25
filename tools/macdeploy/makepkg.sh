d1856cd4c55ae59ec0c71858211a6201717ce4cc
Michael Fisher <mfisher@kushview.net>
2022-08-25 01:03:37 -0400

    osx: add installer helper script

---
+++ tools/macdeploy/makepkg.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+rm -rf build/stage build/dist build/dmg dist
+meson install -C build --destdir="."
+
+python tools/macdeploy/appbundle.py build/stage/lib/vst/KV-Element.vst -verbose 0 -distdir="build/dist" -no-clean
+python tools/macdeploy/appbundle.py build/stage/lib/vst/KV-Element-FX.vst -verbose 0 -distdir="build/dist" -no-clean
+
+python tools/macdeploy/appbundle.py build/stage/lib/vst3/KV-Element.vst3 -verbose 0  -distdir="build/dist" -no-clean
+python tools/macdeploy/appbundle.py build/stage/lib/vst3/KV-Element-FX.vst3 -verbose 0  -distdir="build/dist" -no-clean
+
+python tools/macdeploy/appbundle.py build/stage/lib/au/KV-Element.component -verbose 0  -distdir="build/dist" -no-clean
+python tools/macdeploy/appbundle.py build/stage/lib/au/KV-Element-FX.component -verbose 0  -distdir="build/dist" -no-clean
+python tools/macdeploy/appbundle.py build/stage/lib/au/KV-Element-MFX.component -verbose 0  -distdir="build/dist" -no-clean
+
+python tools/macdeploy/appbundle.py build/stage/Element.app -verbose 0  -distdir="build/dist" -no-clean
+
+/usr/local/bin/packagesbuild -v --build-folder="`pwd`/build/dist" tools/macdeploy/element.pkgproj
