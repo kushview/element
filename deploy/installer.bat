876e1c4489c54f4b7eda958af3d022d0a0a08b87
Michael Fisher <mfisher@lvtk.org>
2023-06-18 20:33:40 -0400

    Fix output archive names

--- deploy/installer.bat
+++ deploy/installer.bat
@@ -3,4 +3,4 @@
 rmdir /s /q archives
 mkdir archives
 cd packages
-@ARCHIVEGEN@ -c 9 -f tar.gz ../archives/pd5-windows-archives.tar.gz net.kushview.element net.kushview.element.lv2 net.kushview.element.lua || exit
+@ARCHIVEGEN@ -c 9 -f tar.gz ../archives/element-windows-archives.tar.gz net.kushview.element net.kushview.element.lv2 net.kushview.element.lua || exit
