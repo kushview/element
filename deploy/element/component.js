fc05c833a03369a6c7523b347b346f62754ecb47
Michael Fisher <mfisher@kushview.net>
2024-01-10 17:03:39 -0500

    remove old install, when 'over installing'. closes #826

--- deploy/element/component.js
+++ deploy/element/component.js
@@ -5,7 +5,7 @@ function extractDir() {
     if (systemInfo.kernelType === "darwin") {
         dir = "/Applications";
     } else if (systemInfo.kernelType === "winnt") {
-        dir = installer.value ("ApplicationsDirX64") + "/Kushview/Element";
+        dir = installer.value("ApplicationsDirX64") + "/Kushview/Element";
     }
     return dir;
 }
@@ -16,52 +16,51 @@ function Component() {
     }
 }
 
-Component.prototype.installerLoaded = function() {
-    // if (installer.addWizardPage (component, "UpdateKeyWidget", QInstaller.Introduction)) {
-    //     var widget = gui.pageWidgetByObjectName("DynamicUpdateKeyWidget");
-    //     widget.windowTitle = "Update Key";
-    //     widget.updateKeyType.currentIndexChanged.connect (this, Component.prototype.updateKeyTypeChanged)
-    // }
+Component.prototype.installerLoaded = function () {
+    gui.pageById(QInstaller.ReadyForInstallation).left
+        .connect(this, Component.prototype.handleUninstallPreviousVersion);
 }
 
-function updateRepositories() {
-    var widget = gui.pageWidgetByObjectName("DynamicUpdateKeyWidget");
-    let tp  = widget.updateKeyType.currentText
-    let key = widget.updateKey.text()
-    console.log (tp, key)
-}
-
-Component.prototype.updateKeyTypeChanged = function (index) {
-   // updateRepositories()
-}
-
-Component.prototype.createOperations = function() {
+Component.prototype.createOperations = function () {
     for (var i = 0; i < component.archives.length; ++i) {
-        let a = component.archives [i]
-        if (! a.endsWith ('7z'))
+        let a = component.archives[i]
+        if (!a.endsWith('7z'))
             continue;
         let target = extractDir();
         if (target.length <= 0)
             continue;
-        component.addElevatedOperation ("Extract", a, target)
+        component.addElevatedOperation("Extract", a, target)
     }
 
     if (isWindows()) {
-        component.addOperation ('CreateShortcut',
+        component.addOperation('CreateShortcut',
             '@ApplicationsDirX64@/Kushview/Element/bin/element.exe',
             '@DesktopDir@/Element.lnk',
             'workingDirectory=@TargetDir@');
-        component.addOperation ('CreateShortcut',
+        component.addOperation('CreateShortcut',
             '@ApplicationsDirX64@/Kushview/Element/bin/element.exe',
             '@StartMenuDir@/Element/Element.lnk',
             'workingDirectory=@TargetDir@');
-        component.addOperation ('CreateShortcut',
+        component.addOperation('CreateShortcut',
             '@TargetDir@/updater.exe',
             '@StartMenuDir@/Element/Updater.lnk',
             'workingDirectory=@TargetDir@');
 
-        exePat = installer.value ("ApplicationsDirX64") + "\\Kushview\\Element\\bin\\element.exe"
-        component.addOperation ('RegisterFileType', 'els', exePat + " '%1'", 'Element Session');
-        component.addOperation ('RegisterFileType', 'elg', exePat + " '%1'", 'Element Graph');
+        exePat = installer.value("ApplicationsDirX64") + "\\Kushview\\Element\\bin\\element.exe"
+        component.addOperation('RegisterFileType', 'els', exePat + " '%1'", 'Element Session');
+        component.addOperation('RegisterFileType', 'elg', exePat + " '%1'", 'Element Graph');
     }
 };
+
+Component.prototype.handleUninstallPreviousVersion = function () {
+    if (!isWindows())
+        return;
+    var dir = installer.value("TargetDir");
+    if (installer.fileExists(dir) && installer.fileExists(dir + "/updater.exe")) {
+        console.log("running uninstall quietly")
+        installer.gainAdminRights();
+        installer.execute (dir + '/updater.exe', ['purge', '-c'])
+    } else {
+        console.log("not running uninstall")
+    }
+}
