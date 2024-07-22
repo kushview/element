02ee688dd76e781504864e89f860bd43d3f39d1a
Michael Fisher <mfisher@lvtk.org>
2024-07-22 05:11:58 -0400

    juce: fix problems with plugin client wrappers.

--- plugins/macvstutils.mm
+++ plugins/macvstutils.mm
@@ -1,3 +1,3 @@
 #include <element/juce/config.h>
 #include "pluginconfig.h"
-#include <juce_audio_plugin_client/juce_audio_plugin_client_VST_utils.mm>
+// #include <juce_audio_plugin_client/juce_audio_plugin_client_VST_utils.mm>
--- plugins/vst2client.cpp
+++ plugins/vst2client.cpp
@@ -1,3 +1,8 @@
 #include <element/juce/config.h>
 #include "pluginconfig.h"
+
+#if __APPLE__
+#include <Cocoa/Cocoa.h>
+#endif
+
 #include <juce_audio_plugin_client/juce_audio_plugin_client_VST2.cpp>
