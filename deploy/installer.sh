fbf12f704cee767c9147431806a2d3eba1a1047c
Michael Fisher <mfisher@lvtk.org>
2024-11-08 10:09:50 -0500

    installer: don't package plugins if not built

--- deploy/installer.sh
+++ deploy/installer.sh
@@ -19,11 +19,14 @@ cd packages && @ARCHIVEGEN@ -f tar.gz -c 9 \
     ../archives/element-osx.tar.gz \
         net.kushview.element \
         net.kushview.element.lua
-@ARCHIVEGEN@ -f tar.gz -c 9 \
-    ../archives/element-plugins-osx.tar.gz \
-        net.kushview.element.au \
-        net.kushview.element.vst \
-        net.kushview.element.vst3
+
+if [ -d "net.kushview.element.vst3" ]; then
+    @ARCHIVEGEN@ -f tar.gz -c 9 \
+        ../archives/element-plugins-osx.tar.gz \
+            net.kushview.element.au \
+            net.kushview.element.vst \
+            net.kushview.element.vst3
+fi
 
 cd ../archives
 md5 *.* > md5.sum
