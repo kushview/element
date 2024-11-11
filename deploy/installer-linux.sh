274e40e7da80162da3817973143b73a3874941c3
Michael Fisher <mfisher@lvtk.org>
2024-11-11 18:21:11 -0500

    linux: installer, skip plugins if not built

--- deploy/installer-linux.sh
+++ deploy/installer-linux.sh
@@ -10,9 +10,12 @@ cd packages && @ARCHIVEGEN@ -f tar.gz -c 9 \
     ../archives/element-osx.tar.gz \
         net.kushview.element \
         net.kushview.element.lua
-@ARCHIVEGEN@ -f tar.gz -c 9 \
-    ../archives/element-plugins-osx.tar.gz \
-        net.kushview.element.vst \
-        net.kushview.element.vst3
+
+if [ -d "net.kushview.element.vst3" ]; then
+    @ARCHIVEGEN@ -f tar.gz -c 9 \
+        ../archives/element-plugins-osx.tar.gz \
+            net.kushview.element.vst \
+            net.kushview.element.vst3
+fi
 
 cd ../archives
