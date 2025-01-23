19d6d96d4ce90c7e8f0fa0b583c640aeb0d1045c
Michael Fisher <880111+mfisher31@users.noreply.github.com>
2025-01-22 22:30:28 -0500

    Fix Not Being Able to Use Greater than 7.1 Channel Sets (#868)
    * 7.1: preparing to re-import bus selection code from JUCE.
    * 7.1: replace old io box with juce copied one (wip)
    * 7.1: working when changing busses to higher channel counts. fixes #796

---
+++ src/ui/ioconfigurationwindow.hpp
@@ -0,0 +1,66 @@
+/*
+  ==============================================================================
+
+   This file is part of the JUCE library.
+   Copyright (c) 2022 - Raw Material Software Limited
+
+   JUCE is an open source library subject to commercial or open-source
+   licensing.
+
+   By using JUCE, you agree to the terms of both the JUCE 7 End-User License
+   Agreement and JUCE Privacy Policy.
+
+   End User License Agreement: www.juce.com/juce-7-licence
+   Privacy Policy: www.juce.com/juce-privacy-policy
+
+   Or: You may also use this code under the terms of the GPL v3 (see
+   www.gnu.org/licenses).
+
+   JUCE IS PROVIDED "AS IS" WITHOUT ANY WARRANTY, AND ALL WARRANTIES, WHETHER
+   EXPRESSED OR IMPLIED, INCLUDING MERCHANTABILITY AND FITNESS FOR PURPOSE, ARE
+   DISCLAIMED.
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include <element/juce/audio_processors.hpp>
+#include <element/node.hpp>
+
+namespace element {
+
+class MainWindow;
+class GraphEditorComponent;
+
+//==============================================================================
+class IOConfigurationWindow final : public  juce::AudioProcessorEditor
+{
+public:
+    IOConfigurationWindow (const Node&, juce::AudioProcessor&);
+    ~IOConfigurationWindow() override;
+
+    //==============================================================================
+    void paint (juce::Graphics& g) override;
+    void resized() override;
+
+private:
+    class InputOutputConfig;
+
+    juce::AudioProcessor::BusesLayout currentLayout;
+    juce::Label title;
+    std::unique_ptr<InputOutputConfig> inConfig, outConfig;
+    Node _node;
+    
+    InputOutputConfig* getConfig (bool isInput) noexcept    { return isInput ? inConfig.get() : outConfig.get(); }
+    void update();
+
+    MainWindow* getMainWindow() const;
+    GraphEditorComponent* getGraphEditor() const;
+    juce::AudioProcessorGraph* getGraph() const;
+    juce::AudioProcessorGraph::NodeID getNodeID() const;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (IOConfigurationWindow)
+};
+
+}
