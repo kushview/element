817a4730cf7f712b8154360a44e62e45e93a8ed3
Michael Fisher <mfisher@kushview.net>
2025-08-25 15:59:19 -0400

    lua: upload and use correct api docs URL

--- src/urls.hpp
+++ src/urls.hpp
@@ -14,10 +14,10 @@
 #define EL_URL_CHANGELOG   "https://github.com/kushview/element/blob/main/CHANGELOG.md"
 
 //==============================================================================
-#define EL_URL_API_HOME "https://kushview.net/api"
+#define EL_URL_DOCS_HOME "https://kushview.net/api"
 #define EL_URL_MANUAL_HOME "https://element.readthedocs.io"
 
 #ifndef EL_URL_API_LUA_EL
-#define EL_URL_API_LUA_EL EL_URL_API_HOME "/el/latest/index.html"
+#define EL_URL_API_LUA_EL "https://kushview.github.io/element-ldoc/"
 #endif
 // clang-format on
