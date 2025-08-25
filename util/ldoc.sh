6757a8ca135bc28937cfd3504e865110b4c64814
Michael Fisher <mfisher@kushview.net>
2025-08-25 12:08:58 -0400

    ldoc: add script to aid generating docs when not using meson.

---
+++ util/ldoc.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+#
+# This file executes ldoc to generate documentation for Lua source files.
+# It should be ran from the top level source directory.
+#
+# Usage: sh util/luadoc.sh
+
+outdir="$(pwd)/build/lua-html"
+rm -rf "${outdir}"
+mkdir -p "${outdir}"
+
+ldoc -f markdown -q -c docs/config.ld -d "${outdir}" .
