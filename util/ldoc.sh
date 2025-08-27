ef06556baf7718430241e7c9608579eb20326fbc
Michael Fisher <mfisher@kushview.net>
2025-08-27 12:11:23 -0400

    ldoc.sh: require absolute output path.

--- util/ldoc.sh
+++ util/ldoc.sh
@@ -1,14 +1,23 @@
-#!/usr/bin/bash
+#!/bin/sh
 #
 # This file executes ldoc to generate documentation for Lua source files.
 # It should be ran from the top level source directory.
 #
 # Usage: sh util/luadoc.sh [output_directory]
 
+srcdir="$(dirname "$(dirname "$(realpath "$0")")")"
 outdir="${1:-$(pwd)/build/lua-html}"
+
+set -e
+
+if [ "${outdir#/}" = "${outdir}" ]; then
+    echo "Error: Output directory must be an absolute path" >&2
+    exit 1
+fi
+
 mkdir -p "${outdir}"
 
-ldoc -f markdown -q -X \
+cd "${srcdir}" && ldoc -f markdown -q -X \
     -c docs/config.ld \
     --multimodule \
     -d "${outdir}" .
