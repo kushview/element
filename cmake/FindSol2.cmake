aa4845095fe08bfa2402bb40cf94cff41be0033c
Michael Fisher <mfisher31@gmail.com>
2026-01-20 07:03:50 -0500

    cmake: find sol2 on system, or use fetch content

---
+++ cmake/FindSol2.cmake
@@ -0,0 +1,8 @@
+find_package(sol2 4.0.0 CONFIG)
+if(NOT sol2_FOUND)
+    FetchContent_Declare(sol2
+        GIT_REPOSITORY https://github.com/ThePhD/sol2.git
+        GIT_TAG c1f95a773c6f8f4fde8ca3efe872e7286afe4444
+        GIT_SHALLOW ON)
+    FetchContent_MakeAvailable(sol2)
+endif()
