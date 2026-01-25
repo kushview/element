c93198a321d904d3779062f5842f85edf668d9a5
Michael Fisher <mfisher31@gmail.com>
2026-01-25 16:54:24 -0500

    sol2: add exclude from all flag. avoid installing

--- cmake/FindSol2.cmake
+++ cmake/FindSol2.cmake
@@ -6,6 +6,7 @@ if(NOT sol2_FOUND)
     FetchContent_Declare(sol2
         GIT_REPOSITORY https://github.com/ThePhD/sol2.git
         GIT_TAG c1f95a773c6f8f4fde8ca3efe872e7286afe4444
-        GIT_SHALLOW ON)
+        GIT_SHALLOW ON
+        EXCLUDE_FROM_ALL)
     FetchContent_MakeAvailable(sol2)
 endif()
