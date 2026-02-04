a57ee95d6061d2b302dd54ab14571dc8284a467d
Michael Fisher <mfisher31@gmail.com>
2026-02-04 16:11:31 -0500

    refactor: extract sol2 repository based on cmake version

--- cmake/FindSol2.cmake
+++ cmake/FindSol2.cmake
@@ -3,10 +3,21 @@
 
 find_package(sol2 4.0.0 CONFIG)
 if(NOT sol2_FOUND)
-    FetchContent_Declare(sol2
-        GIT_REPOSITORY https://github.com/ThePhD/sol2.git
-        GIT_TAG c1f95a773c6f8f4fde8ca3efe872e7286afe4444
-        GIT_SHALLOW ON
-        EXCLUDE_FROM_ALL)
-    FetchContent_MakeAvailable(sol2)
+    set(ELEMENT_SOL2_REPO "https://github.com/ThePhD/sol2.git")
+    set(ELEMENT_SOL2_REVISION "c1f95a773c6f8f4fde8ca3efe872e7286afe4444")
+    if(CMAKE_VERSION VERSION_GREATER_EQUAL 3.28)
+        FetchContent_Declare(sol2
+            GIT_REPOSITORY ${ELEMENT_SOL2_REPO}
+            GIT_TAG ${ELEMENT_SOL2_REVISION}
+            GIT_SHALLOW ON
+            EXCLUDE_FROM_ALL)
+        FetchContent_MakeAvailable(sol2)
+    else()
+        FetchContent_Declare(sol2
+            GIT_REPOSITORY ${ELEMENT_SOL2_REPO}
+            GIT_TAG ${ELEMENT_SOL2_REVISION}
+            GIT_SHALLOW ON)
+        FetchContent_Populate(sol2)
+        add_subdirectory(${sol2_SOURCE_DIR} ${sol2_BINARY_DIR} EXCLUDE_FROM_ALL)
+    endif()
 endif()
